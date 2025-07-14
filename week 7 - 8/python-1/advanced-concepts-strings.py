17e39def9eb858a60b75c17599e6511fb2644494
Malekhotla <malekhotlapheko@gmail.com>
2025-07-14 22:27:55 +0200

    Add files via upload

---
+++ week 7 - 8/python-1/advanced-concepts-strings.py
@@ -0,0 +1,7 @@
+#Advanced concepts - strings
+
+message = ' Hello, World! '
+
+print(message.strip())# Remove leading and trailing whitespace
+print(message.lower())# Convert all characters to lowercase
+print(message.split(','))# split the string into a list based on the coma
---
+++ week 7 - 8/python-1/variable-python.py
@@ -0,0 +1,9 @@
+#variable
+
+my_variable = 10
+total = 0
+user = 'John'
+
+#Invalid
+second_variable = 10
+user_name = 20
